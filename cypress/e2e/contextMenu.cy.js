93bf9e906b22da0937c95345fa880b1e741c91be
Santiag MDO <mdosantiago2@gmail.com>
2024-03-27 19:56:39 +0100

    Changing txt

--- cypress/e2e/contextMenu.cy.js
+++ cypress/e2e/contextMenu.cy.js
@@ -14,12 +14,19 @@ describe("pruebas-contextMenu", () => {
     });
 
     it('Context Menu click and alert', () => {
+        
+        
         // Simular un clic derecho en un elemento que generará una alerta
-          cy.get('#hot-spot').rightclick();
-    
-          // Que aparezca la alerta y confirmarla
+          cy
+            .get('#hot-spot')
+            .rightclick()
+            //.get('[data-cy="alert-body"]')
+            //.invoke('show')
+
+              // Que aparezca la alerta y confirmarla
           cy.on('window:alert', (alertText) => {
           expect(alertText).to.equal('You selected a context menu');
           });
+    
     });
 });
